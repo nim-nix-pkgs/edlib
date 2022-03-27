217a9099b4701266edcaf43d7747c8da00a854ef
hugosenari <hugosenari@gmail.com>
2022-03-27 18:15:09 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-edlib-main.ref   = "refs/heads/main";
   inputs.src-edlib-main.owner = "bio-nim";
   inputs.src-edlib-main.repo  = "nim-edlib";
+  inputs.src-edlib-main.dir   = "";
   inputs.src-edlib-main.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
