8fed3f5f5a0d765e9d0f4f335eb97b85de98a4ae
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:15 -0300

    chore: re index edlib

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."edlib-main".dir   = "main";
+  inputs."edlib-main".owner = "nim-nix-pkgs";
+  inputs."edlib-main".ref   = "master";
+  inputs."edlib-main".repo  = "edlib";
+  inputs."edlib-main".type  = "github";
+  inputs."edlib-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."edlib-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
